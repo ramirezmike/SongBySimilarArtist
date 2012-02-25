45edc11dc9e61a5579e22d9ecb5dcc49e23e0f9c
Michael Ramirez <ramirezmike2@gmail.com>
2012-02-25 08:19:55 -0500

    TinySong Parsing Complete

--- gss.py
+++ gss.py
@@ -2,16 +2,15 @@ import argparse
 import urllib
 from BeautifulSoup import BeautifulSoup
 import random
+import json
 parser = argparse.ArgumentParser(description='Add Key.')
 parser.add_argument('key',type=str)
 arguments = parser.parse_args()
-lastfm_key2 = 'f30074d1365071a86b89594c8d583658'
 lastfm_key = arguments.key
 
 artistList = []
 url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=kate+nash&api_key=" + lastfm_key)
 string_url = url.read()
-url.close()
 soup = BeautifulSoup(string_url)
 print "soup made"
 for tag in soup.findAll("name"):
@@ -20,4 +19,9 @@ for tag in soup.findAll("name"):
 
 randomIndex = random.randint(0,len(artistList)-1)
 randomArtist = artistList[randomIndex]
-print randomArtist
+randomArtist = randomArtist.replace(' ','+')
+tinysongurl = urllib.urlopen("http://tinysong.com/s/" + randomArtist + "?format=json&limit=1&key=59f18b16a371c3d6090205c642fdf0f5").read()
+print tinysongurl
+parsed_tinysongurl = json.loads(tinysongurl) 
+pickedSong_url = parsed_tinysongurl[0]['Url']
+print pickedSong_url
