b133a9277ac7efcba803bde581c992e84bc29e77
Michael Ramirez <ramirezmike2@gmail.com>
2012-02-25 00:34:01 -0500

    Last.fm API working

--- gss.py
+++ gss.py
@@ -1,1 +1,12 @@
-print "Hello world!"
+import urllib
+from BeautifulSoup import BeautifulSoup
+
+url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=kate+nash&api_key=f30074d1365071a86b89594c8d583658")
+string_url = url.read()
+url.close()
+soup = BeautifulSoup(string_url)
+print "soup made"
+print soup
+for tag in soup.findAll("name"):
+	if tag(text=True):
+		print tag.text.replace('&amp;','&')
