630e50a2e8510b15c5d1129c2a1b94e45d898bb1
Michael Ramirez <ramirezmike2@gmail.com>
2012-02-25 01:52:28 -0500

    Added Argument Parsing

--- gss.py
+++ gss.py
@@ -1,9 +1,15 @@
+import argparse
 import urllib
 from BeautifulSoup import BeautifulSoup
 import random
+parser = argparse.ArgumentParser(description='Add Key.')
+parser.add_argument('key',type=str)
+arguments = parser.parse_args()
+lastfm_key2 = 'f30074d1365071a86b89594c8d583658'
+lastfm_key = arguments.key
 
 artistList = []
-url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=kate+nash&api_key=f30074d1365071a86b89594c8d583658")
+url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=kate+nash&api_key=" + lastfm_key)
 string_url = url.read()
 url.close()
 soup = BeautifulSoup(string_url)
@@ -13,4 +19,5 @@ for tag in soup.findAll("name"):
 		artistList.append(tag.text.replace('&amp;','&'))
 
 randomIndex = random.randint(0,len(artistList)-1)
-print artistList[randomIndex]
+randomArtist = artistList[randomIndex]
+print randomArtist
