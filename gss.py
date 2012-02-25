78783a1d9c9d03d4e81c8121fb945ae40f536425
Michael Ramirez <ramirezmike2@gmail.com>
2012-02-25 01:25:59 -0500

    Last.fm API -> Single Random Similar Artist Complete

--- gss.py
+++ gss.py
@@ -1,12 +1,16 @@
 import urllib
 from BeautifulSoup import BeautifulSoup
+import random
 
+artistList = []
 url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=kate+nash&api_key=f30074d1365071a86b89594c8d583658")
 string_url = url.read()
 url.close()
 soup = BeautifulSoup(string_url)
 print "soup made"
-print soup
 for tag in soup.findAll("name"):
 	if tag(text=True):
-		print tag.text.replace('&amp;','&')
+		artistList.append(tag.text.replace('&amp;','&'))
+
+randomIndex = random.randint(0,len(artistList)-1)
+print artistList[randomIndex]
