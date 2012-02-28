41c78b2031552de3728611614ffbd09c2f7f554e
Michael Ramirez <ramirezmike2@gmail.com>
2012-02-27 21:41:27 -0500

    Browser Control + Grooveshark Javascript Functional

--- gss.py
+++ gss.py
@@ -3,25 +3,62 @@ import urllib
 from BeautifulSoup import BeautifulSoup
 import random
 import json
-parser = argparse.ArgumentParser(description='Add Key.')
-parser.add_argument('key',type=str)
-arguments = parser.parse_args()
-lastfm_key = arguments.key
+import pexpect
+import time
+
+argParser = argparse.ArgumentParser(description='Add Key.')
+argParser.add_argument('key',type=str)
+arguments = argParser.parse_args()
+passedArtist = arguments.key.replace(' ','+')
+print passedArtist
+lastfm_key = "f30074d1365071a86b89594c8d583658" 
 
 artistList = []
-url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=kate+nash&api_key=" + lastfm_key)
+print "LastFM Opening"
+url = urllib.urlopen("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=" + passedArtist + "&api_key=" + lastfm_key)
+print "LastFM Opened"
 string_url = url.read()
 soup = BeautifulSoup(string_url)
-print "soup made"
+print "Soup Made"
 for tag in soup.findAll("name"):
 	if tag(text=True):
 		artistList.append(tag.text.replace('&amp;','&'))
 
-randomIndex = random.randint(0,len(artistList)-1)
-randomArtist = artistList[randomIndex]
+randomIndexForArtistList = random.randint(0,len(artistList)-1)
+randomArtist = artistList[randomIndexForArtistList]
 randomArtist = randomArtist.replace(' ','+')
+
+print "TinySong Opening"
 tinysongurl = urllib.urlopen("http://tinysong.com/s/" + randomArtist + "?format=json&limit=1&key=59f18b16a371c3d6090205c642fdf0f5").read()
-print tinysongurl
+print "TinySong Opened"
 parsed_tinysongurl = json.loads(tinysongurl) 
+print parsed_tinysongurl
 pickedSong_url = parsed_tinysongurl[0]['Url']
 print pickedSong_url
+
+
+browserController = pexpect.spawn('/usr/bin/irb')
+browserController.expect('>>')
+print 'Browser Controller Initiated'
+browserController.sendline('require "watir-webdriver"')  
+browserController.expect('>>')
+browserController.sendline('browser = Watir::Browser.new :ff')  
+browserController.expect('>>')
+browserController.sendline('browser.goto "' + pickedSong_url + '"')  
+browserController.expect('>>')
+time.sleep(20)
+browserController.sendline('browser.execute_script("javascript:window.Grooveshark.addSongsByID(21104374,false);")')
+print "First Javascript Insertion"
+time.sleep(10)
+browserController.expect('>>')
+browserController.sendline('browser.execute_script("javascript:window.Grooveshark.addSongsByID(21104374,false);")')
+browserController.expect('>>')
+print "Second Javascript Insertion"
+time.sleep(10)
+browserController.sendline('browser.execute_script("javascript:window.Grooveshark.addSongsByID(21104374,false);")')
+browserController.expect('>>')
+print "Third Javascript Insertion"
+time.sleep(10)
+browserController.sendline('browser.execute_script("javascript:window.Grooveshark.addSongsByID(21104374,false);")')
+browserController.expect('>>')
+print "Fourth Javascript Insertion"
